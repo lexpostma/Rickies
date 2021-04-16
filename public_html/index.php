671abb8b68e0899db4426af4487f1d3e2d42cc25
Lex Postma <lex@postma.me>
2021-04-16 22:13:15 +0200

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,2 @@
+<h1>The Rickies</h1>
+<p>Hello world! Fan-art website for The Rickies coming soon-ish. Just setting everything up. Enjoy the <a href="https://relay.fm/connected">Connected</a> podcast on Relay FM</p>
