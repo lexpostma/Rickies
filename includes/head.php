6a263c453db80de45a80a0bb9d173e66d538bc59
Lex Postma <lex@postma.me>
2021-04-21 08:57:06 +0200

    Set up initial skeleton

---
+++ includes/head.php
@@ -0,0 +1,95 @@
+<?
+	$seo_title = "The Rickies";
+	if($environment !== "production") {
+		$seo_title .= " [".$environment."]";
+	}
+?>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no, viewport-fit=cover" />
+<title><?=$seo_title?></title>
+<base href="<?=base_url()?>">
+
+<!-- Icons -->
+<link rel="shortcut icon" type="image/ico" href="favicon.png" />
+<link rel="mask-icon" href="images/safari.svg" color="#106DC6">
+
+<!-- Standard SEO / Google -->
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="Lex Postma" />
+<meta name="copyright" content="Copyright (c) 2020-<?=date('Y')?> by Lex Postma" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<?
+	if($environment !== "production") {
+		echo '<meta name="robots" content="noindex,nofollow" />';
+	}
+?>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-77702212-2"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-77702212-2');
+</script> -->
+
+<!-- Open Graph protocol / Facebook -->
+<meta property="og:title" content="The Rickies" />
+<meta property="og:description" content="" />
+<meta property="og:url" content="<?=current_url()?>" />
+<meta property="og:image" content=""/>
+<meta property="og:image:secure_url" content=""/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="The Rickies" />
+<meta property="fb:admins" content="1308188724" />
+
+<!-- Twitter (Cards) -->
+<meta name="twitter:widgets:link-color" content="#106DC6" />
+<meta name="twitter:url" content="<?=current_url()?>">
+<meta name="twitter:title" content="The Rickies">
+<meta name="twitter:description" content="">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:image:src" content="">
+<meta name="twitter:creator" content="@lexpostma">
+<meta name="twitter:site" content="@_connectedfm">
+
+<?
+	// include("ios_optimisation.php");
+?>
+
+<!-- Normalize and Google Font		 -->
+<link rel="stylesheet" href="/styles/normalize.css">
+<!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet"> -->
+
+<!-- Style sheet -->
+<link rel="stylesheet" href="/styles/global.css?v=<?=date('z')?>">
+
+<!-- If it's a webapp, open links inside webapp instead of Safari https://stackoverflow.com/a/8173161 -->
+<!-- <script>
+	(
+		function(element,userAgent,userAgentVar){
+			if(userAgentVar in userAgent&&userAgent[userAgentVar]){
+				var d,
+					elementLocation=element.location,
+					f=/^(a|html)$/i;
+	
+				element.addEventListener("click",function(element){
+					elementTarget=element.target;
+					while(!f.test(elementTarget.nodeName))elementTarget=elementTarget.parentNode;
+	
+					// links with target="_blank" should still open in Safari
+					var newTabLink = elementTarget.attributes.getNamedItem("target");
+					if(newTabLink && newTabLink.value == "_blank") return;
+	
+					// links that should with # in href should have default behaviour
+					var anchorLink = elementTarget.attributes.getNamedItem("href");
+					if(anchorLink && anchorLink.value.startsWith("#")) return;
+	
+					"href"in elementTarget&&(elementTarget.href.indexOf("http")||~elementTarget.href.indexOf(elementLocation.host))&&(element.preventDefault(),elementLocation.href=elementTarget.href)
+				},!1)
+			}
+		}
+	)(document,window.navigator,"standalone")
+</script> -->
--- public_html/index.php
+++ public_html/index.php
@@ -2,36 +2,9 @@
 <html lang="en">
 	<head>
 		<title>The Rickies</title>
-		<style>
-			*, *:before, *:after {
-				-webkit-box-sizing: border-box;
-				   -moz-box-sizing: border-box;
-						box-sizing: border-box;
-			}
-			
-			html {
-				font-family: sans-serif;
-				color: #EEE;
-				background: #2e2e2e;
-				text-align: center;
-				width: 100%;
-				height: 100%;
-				line-height: 1.5;
-			}
-			
-			body {
-				max-width: 500px;
-				width: 100%;
-				margin: 50px auto 0;
-				padding: 20px;
-			}
-			a {
-				color: white;
-			}
-		</style>
 	</head>
 	<body>
 		<h1>The Rickies</h1>
-		<p>Hello world! Fan-art website for The Rickies coming soon-ish. Just setting everything up. Enjoy the <a href="https://relay.fm/connected">Connected</a> podcast on Relay FM</p>
+		<p>Hello world!</p>
 	</body>
 </html>
