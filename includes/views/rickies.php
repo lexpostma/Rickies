c20a7a57e24440f042c58f5ebb0007bd84f93475
Lex Postma <lex@postma.me>
2021-05-12 17:26:11 +0200

    Adjust Bill Of URL to test webapp

--- includes/views/rickies.php
+++ includes/views/rickies.php
@@ -21,7 +21,7 @@
 	<div class="nav_content--items">
 		<a class="active" href="#list"><span class="need_space">The </span>Rickies</a>
 		<a href="/leaderboard"><span class="need_space">Host </span>Leaderboard</a>
-		<a href="<?= billofrickies_url() ?>">The Bill of Rickies</a>
+		<a href="/billof">The Bill of Rickies</a>
 		<a href="/about">About</a>
 	</div>
 </nav>
