b2db76aca11f1e5de4bb788b66f95447b905f4ca
Lex Postma <lexpostma@users.noreply.github.com>
2021-05-13 07:27:05 +0200

    Update About items and artwork

--- includes/view_controllers/about_controller.php
+++ includes/view_controllers/about_controller.php
@@ -6,31 +6,38 @@
 $resources = [
 	[
 		'url' => 'https://rickies.net',
-		'img_url' => '/images/bill-of-rickies-avatar.png',
+		'img_url' => '/images/about/rickipedia.png',
 		'label1' => 'Rickipedia',
 		// 'label2' => 'By Jason Biatek',
 		'label3' => 'By Jason Biatek',
 	],
 	[
 		'url' => 'https://www.notion.so/Connected-262725156a0041bd9b0248c172862cb0',
-		'img_url' => '/images/bill-of-rickies-avatar.png',
+		'img_url' => '/images/about/notion.png',
 		'label1' => 'Notion database',
 		// 'label2' => 'By Jason Biatek',
 		// 'label3' => '25 April 2020',
 	],
 	[
 		'url' => 'https://twitter.com/kingtritium/status/1278081027387392006?s=21',
-		'img_url' => '/images/bill-of-rickies-avatar.png',
+		'img_url' => '/images/about/bill-of-rickies-mathias.png',
 		'label1' => 'The Bill of Rickies',
 		// 'label2' => '25 April 2020',
 		'label3' => 'By Mathias Bruggemann',
 	],
 	[
 		'url' => 'https://github.com/lexpostma/Rickies/blob/main/Acknowledgements.md',
-		'img_url' => '/images/bill-of-rickies-avatar.png',
+		'img_url' => '/images/about/acknowledgements.png',
 		'label1' => 'Acknowledgements',
 		// 'label2' => 'By Jason Biatek',
 		'label3' => 'GitHub',
+	],
+	[
+		'url' => 'https://getconnectedpro.co',
+		'img_url' => '/images/connected-pro-artwork.png',
+		'label1' => 'Get Connected Pro',
+		'label2' => 'Preshow, postshow, no ads.',
+		// 'label3' => 'GitHub',
 	],
 ];
 
