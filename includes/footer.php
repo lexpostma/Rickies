65ea52147196e168667b7ef235cce22eb6013ef6
Lex Postma <lex@postma.me>
2021-05-14 14:19:50 +0200

    Promote webapp from footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,38 @@
 <div class="footer_space"></div>
-<footer>
+<footer id="footer">
 	<p>Designed and built with <?= random(['💚', '💛', '🧡', '❤️', '💜', '💙']) ?> by <a target="_blank" href="<?= $head[
  	'site_lex'
  ] ?>" <?= $head['site_lex_goat'] ?>><?= $head[
 	'author'
 ] ?></a>,<br />listener and fan of the <a target="_blank" href="<?= $head['site_connected'] ?>" <?= $head[
 	'site_connected_goat'
-] ?>>Connected</a> podcast</p>
-	 <a target="_blank" href="<?= $head['site_lex'] ?>" <?= $head['site_lex_goat'] ?> id="footer_corner">
-		 <img src="/images/lex-logo.svg" alt="<?= $head['author'] ?> logo"/>
-	 </a>
+] ?>>Connected</a> podcast.</p>
+	<p class="promote_webapp">This website is designed as a web app. Add it to your home screen for the best experience.</p>
+	<p class="refresh_page">Refresh this page</p>
+	<a target="_blank" href="<?= $head['site_lex'] ?>" <?= $head['site_lex_goat'] ?> id="footer_corner">
+		<img src="/images/lex-logo.svg" alt="<?= $head['author'] ?> logo"/>
+    </a>
 </footer>
+
+<script>
+	let footer = document.getElementById('footer');
+
+	function promote_standalone() {
+		if(
+			(navigator.userAgent.indexOf('iPhone') != -1) ||
+			(navigator.userAgent.indexOf('iPad') != -1)
+		) {
+			if(window.navigator.standalone == true){
+				// Added to home screen, offer refresh
+				footer.classList.add('standalone');
+			} else {
+				// Not added to home screen, promote webapp
+				footer.classList.add('standalonable');
+			}
+		} else {
+			// Not iOS
+		}
+	}
+
+	promote_standalone()
+</script>
