9c03747e25796de04e87d6060f66916c53e82d91
Lex Postma <lex@postma.me>
2021-05-14 14:33:47 +0200

    Update web app promotion

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
 <div class="footer_space"></div>
 <footer id="footer">
+	<p class="promote_webapp">This website is designed as a web app.<br />Add it to your home screen for the best experience.</p>
+	<p class="refresh_page"><a href="#" onclick="window.location.href=window.location.href">Refresh this page</a></p>
 	<p>Designed and built with <?= random(['💚', '💛', '🧡', '❤️', '💜', '💙']) ?> by <a target="_blank" href="<?= $head[
  	'site_lex'
  ] ?>" <?= $head['site_lex_goat'] ?>><?= $head[
@@ -7,8 +9,6 @@
 ] ?></a>,<br />listener and fan of the <a target="_blank" href="<?= $head['site_connected'] ?>" <?= $head[
 	'site_connected_goat'
 ] ?>>Connected</a> podcast.</p>
-	<p class="promote_webapp">This website is designed as a web app. Add it to your home screen for the best experience.</p>
-	<p class="refresh_page">Refresh this page</p>
 	<a target="_blank" href="<?= $head['site_lex'] ?>" <?= $head['site_lex_goat'] ?> id="footer_corner">
 		<img src="/images/lex-logo.svg" alt="<?= $head['author'] ?> logo"/>
     </a>
@@ -24,10 +24,10 @@
 		) {
 			if(window.navigator.standalone == true){
 				// Added to home screen, offer refresh
-				footer.classList.add('standalone');
+				footer.classList.add('is_standalone');
 			} else {
-				// Not added to home screen, promote webapp
-				footer.classList.add('standalonable');
+				// Not added to home screen, promote web app
+				footer.classList.add('isnot_standalone');
 			}
 		} else {
 			// Not iOS
