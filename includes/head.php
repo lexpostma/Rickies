c7ce29ea9bfa38c43c1a780ff23793aa0f1cb17a
Lex Postma <lex@postma.me>
2021-05-19 21:36:34 +0200

    Refactor SASS

--- includes/head.php
+++ includes/head.php
@@ -64,10 +64,9 @@ if ($environment !== 'production') {
 
 <!-- Style sheet -->
 <!-- TODO: put date() back to 'z' in URLs to allow caching daily instead of seconds -->
-<link rel="stylesheet" href="/styles/global.css?v=<?= date('s') ?>">
-<link rel="stylesheet" href="/styles/<?= $focus_site ?>.css?v=<?= date('s') ?>">
+<link rel="stylesheet" href="/styles/<?= $focus_site ?>.css?v=<?= date($refresh) ?>">
 <noscript>
-	<link rel="stylesheet" href="/styles/noscript.css?v=<?= date('s') ?>">
+	<link rel="stylesheet" href="/styles/noscript.css?v=<?= date($refresh) ?>">
 </noscript>
 
 <!-- Goat Counter -->
