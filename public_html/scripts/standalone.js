99c3c52c5837d72795a5c569cb754fda242e8f31
Lex Postma <lex@postma.me>
2021-05-21 09:36:22 +0200

    Offer reload on any standalone user-agent

--- public_html/scripts/standalone.js
+++ public_html/scripts/standalone.js
@@ -5,16 +5,15 @@ function refresh_inprogress(el) {
 function promote_standalone() {
 	console.log('Agent: ' + navigator.userAgent);
 	console.log('Standalone: ' + window.navigator.standalone);
-	if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('iPad') != -1) {
-		if (window.navigator.standalone == true) {
-			// Added to home screen, offer refresh
-			document.getElementById('refresh_page').style.display = 'block';
-		} else {
-			// Not added to home screen, promote web app
-			document.getElementById('promote_webapp').style.display = 'block';
-			// Can be overwritten by lack of share sheet support of user-agent
-			// See /scripts/share_button.js
-		}
+
+	if (window.navigator.standalone == true) {
+		// Is on the home screen, offer refresh
+		document.getElementById('refresh_page').style.display = 'block';
+	} else if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('iPad') != -1) {
+		// Not added to home screen, promote web app for iOS
+		document.getElementById('promote_webapp').style.display = 'block';
+		// Can be overwritten by lack of share sheet support of user-agent
+		// See /scripts/share_button.js
 	} else {
 		// Not iOS
 	}
