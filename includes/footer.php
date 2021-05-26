8d92291f7a024697233bc7db34c2f6be0b29f1d0
Lex Postma <lex@postma.me>
2021-05-26 20:51:57 +0200

    Add sixheart to footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <footer id="footer">
 	<p id="promote_webapp">This website is designed as a web app.<br />Add it to your home screen for the best experience.</p>
 	<p id="refresh_page"><button title="Refresh page" class="clean js_link" type="button" data-goatcounter-click="Refresh page" data-goatcounter-referrer="<?= current_url() ?>" onclick="refresh_inprogress(this), location.reload()">Refresh this page</button></p>
-	<p>Designed and built with <?= random(['💚', '💛', '🧡', '❤️', '💜', '💙']) ?> by <a target="_blank" href="<?= $head[
+	<p>Designed and built with <img src="/images/sixheart.png" class="img_emoji" alt="love" /> by <a target="_blank" href="<?= $head[
  	'site_lex'
  ] ?>" <?= $head['site_lex_goat'] ?>><?= $head[
 	'author'
