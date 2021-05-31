59a55b2da95295bc4dfba5a23dc1ca5334daa0b3
Lex Postma <lex@postma.me>
2021-05-31 11:21:50 +0200

    Use SVG for Relay logo

--- includes/variables.php
+++ includes/variables.php
@@ -56,7 +56,7 @@ if ($environment == 'production') {
 	$head_defaults['site_goatcounter'] = 'https://rickies.goatcounter.com';
 	$github = 'main';
 	// NOTE: update for new releases
-	$refresh = 'v1.2.2';
+	$refresh = 'v1.2.3';
 } else {
 	$head_defaults['site_goatcounter'] = 'https://rickies-test.goatcounter.com';
 	$github = 'development';
