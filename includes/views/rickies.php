844848221d680dfcbce88d592e52931b83ed8fe4
Lex Postma <lex@postma.me>
2021-10-13 14:55:59 +0200

    Add archive page

--- includes/views/rickies.php
+++ includes/views/rickies.php
@@ -23,6 +23,7 @@
 		<a class="active" href="#list"><span class="need_space--sm">The </span>Rickies</a>
 		<a href="/billof"><span class="need_space--xs">The </span>Bill of Rickies</a>
 		<a href="/leaderboard"><span class="need_space--sm">Host </span>Leaderboard</a>
+		<a href="/archive">Archive</a>
 		<a href="/about">About</a>
 	</div>
 </nav>
