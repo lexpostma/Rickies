48113b998fdcc4131fb7b6aece6d4e165e8786ea
Lex Postma <lex@postma.me>
2021-10-13 19:37:40 +0200

    Disable scrolling when search is open

--- public_html/scripts/search.js
+++ public_html/scripts/search.js
@@ -7,9 +7,25 @@ function toggle_search() {
 		searchbox.classList.remove('open');
 		toggle_button.classList.remove('open');
 		search_field.blur();
+		enableScroll();
 	} else {
 		searchbox.classList.add('open');
 		toggle_button.classList.add('open');
 		search_field.focus();
+		disableScroll();
 	}
+}
+
+function disableScroll() {
+	// Get the current page scroll position
+	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	(scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+		// if any scroll is attempted, set this to the previous value
+		(window.onscroll = function () {
+			window.scrollTo(scrollLeft, scrollTop);
+		});
+}
+
+function enableScroll() {
+	window.onscroll = function () {};
 }
