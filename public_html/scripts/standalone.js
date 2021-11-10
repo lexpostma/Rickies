8d57b24c1306bb7442255e6cf2f4f17686ef4d87
Lex Postma <lex@postma.me>
2021-11-10 16:30:05 +0100

    Enable actual reload

--- public_html/scripts/standalone.js
+++ public_html/scripts/standalone.js
@@ -1,15 +1,15 @@
 function promote_standalone() {
 	if (window.navigator.standalone == true) {
-		// Is on the home screen, offer pull-to-refresh
+		// Web app is on the home screen, enable pull-to-refresh
 		PullToRefresh.init({
 			mainElement: '.container',
 			onRefresh: function () {
-				alert('refresh');
-				// window.location.reload();
+				// alert('refresh');
+				window.location.reload();
 			},
 		});
 	} else if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('iPad') != -1) {
-		// Not added to home screen, promote web app for iOS
+		// Web app is not added to home screen, promote web app for iOS
 		if (document.getElementById('promote_webapp')) {
 			document.getElementById('promote_webapp').style.display = 'block';
 		}
