52819041100a09b60911348f0dade1b0424bd81d
Lex Postma <lex@postma.me>
2021-11-21 21:50:37 +0100

    Allow dragging to move timeline

--- public_html/scripts/timeline.js
+++ public_html/scripts/timeline.js
@@ -20,3 +20,31 @@ function timeline_zoom(direction) {
 document.addEventListener('DOMContentLoaded', function (event) {
 	timeline_content.scrollLeft = timeline_content.scrollWidth;
 });
+
+// Allow drag to scroll on timeline
+let isDown = false;
+let startX;
+let scrollLeft;
+
+timeline_content.addEventListener('mousedown', (e) => {
+	isDown = true;
+	// timeline_content.classList.add('active');
+	startX = e.pageX - timeline_content.offsetLeft;
+	scrollLeft = timeline_content.scrollLeft;
+});
+timeline_content.addEventListener('mouseleave', () => {
+	isDown = false;
+	// timeline_content.classList.remove('active');
+});
+timeline_content.addEventListener('mouseup', () => {
+	isDown = false;
+	// timeline_content.classList.remove('active');
+});
+timeline_content.addEventListener('mousemove', (e) => {
+	if (!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - timeline_content.offsetLeft;
+	const walk = (x - startX) * 3; //scroll-fast
+	timeline_content.scrollLeft = scrollLeft - walk;
+	console.log(walk);
+});
