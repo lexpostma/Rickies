b2830fad4ddec5de3aab35addc7003326b065aeb
Lex Postma <lex@postma.me>
2021-11-22 09:36:25 +0100

    Add "minimal-ui" to check in iOS 15

--- includes/head.php
+++ includes/head.php
@@ -15,7 +15,7 @@ if ($environment !== 'production') {
 ?>
 
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no, viewport-fit=cover" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no, viewport-fit=cover, minimal-ui" />
 <title><?= $head['title'] ?></title>
 <base href="<?= $head['canonical'] ?>">
 <link rel="canonical" href="<?= $head['canonical'] ?>" />
