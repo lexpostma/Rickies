f222a62eb8b534811f4ff72cc11742a098ffcd7d
Lex Postma <lex@postma.me>
2021-11-27 15:27:55 +0100

    Define timeline scale labels

--- public_html/scripts/timeline.js
+++ public_html/scripts/timeline.js
@@ -3,6 +3,7 @@ const zoomin_button = document.getElementById('zoomin_button');
 const zoomout_button = document.getElementById('zoomout_button');
 const defaultZoom = getComputedStyle(timeline).getPropertyValue('--day-scale');
 const timeline_content = document.getElementsByClassName('timeline--content')[0];
+const first_month = document.getElementsByClassName('month')[0];
 
 function timeline_zoom(direction) {
 	var currentWidth = getComputedStyle(timeline).getPropertyValue('--day-scale');
@@ -16,7 +17,7 @@ function timeline_zoom(direction) {
 		newWidth = defaultZoom;
 	}
 	timeline.style.setProperty('--day-scale', newWidth);
-	console.log('old: ' + currentWidth + ' • new: ' + newWidth);
+	// console.log('old: ' + currentWidth + ' • new: ' + newWidth);
 
 	if (newWidth > 5) {
 		// Disable zoom in button
@@ -31,12 +32,53 @@ function timeline_zoom(direction) {
 		zoomin_button.disabled = false;
 		zoomout_button.disabled = false;
 	}
+
+	set_timeline_scale_labels();
+}
+
+function set_timeline_scale_labels() {
+	// Should be the same as var.$hover-duration in CSS
+	// Timeout is needed because the animation delays the width measurement
+	var delayInMilliseconds = 300;
+
+	setTimeout(function () {
+		// console.log(first_month.offsetWidth);
+		if (first_month.offsetWidth < 20) {
+			timeline_content.classList.add('month_xx');
+			timeline_content.classList.remove('month_sm');
+			timeline_content.classList.remove('month_md');
+		} else if (first_month.offsetWidth < 32) {
+			timeline_content.classList.add('month_sm');
+			timeline_content.classList.remove('month_xx');
+			timeline_content.classList.remove('month_md');
+		} else if (first_month.offsetWidth < 60) {
+			timeline_content.classList.add('month_md');
+			timeline_content.classList.remove('month_xx');
+			timeline_content.classList.remove('month_sm');
+		} else {
+			timeline_content.classList.remove('month_xx');
+			timeline_content.classList.remove('month_sm');
+			timeline_content.classList.remove('month_md');
+		}
+	}, delayInMilliseconds);
 }
 
 // Make sure the end of the timeline is in view on load
 // Via: https://stackoverflow.com/a/56747348
+// And the labels are correctly visible
 document.addEventListener('DOMContentLoaded', function (event) {
 	timeline_content.scrollLeft = timeline_content.scrollWidth;
+	set_timeline_scale_labels();
+});
+
+window.addEventListener('resize', function () {
+	set_timeline_scale_labels();
+});
+
+// When timeline is hidden on mobile, and than resized, it defines label size at 0
+// So when timeline is visible again, it should recalculate the size
+document.getElementById('menu_timeline').addEventListener('click', function () {
+	set_timeline_scale_labels();
 });
 
 // Allow drag to scroll on timeline
