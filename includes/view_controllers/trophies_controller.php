c62717191fd5cbcc535da05013dd5cfc1d2d3c9e
Lex Postma <lex@postma.me>
2022-03-22 22:24:32 +0100

    Improve SEO

--- includes/view_controllers/trophies_controller.php
+++ includes/view_controllers/trophies_controller.php
@@ -7,7 +7,7 @@ $magtrickies = file_get_contents($incl_path . 'magtrickies.html');
 
 $head_custom = [
 	'title' => 'Trickies, the Rickies trophies',
-	'description' => 'The different Rickies trophies explained.',
+	'description' => 'The different Rickies trophies explained, from Tricky to MagTricky.',
 	'image' => domain_url() . '/images/seo/hero-trophies.jpg',
 	'keywords' => ['tricky', 'magtricky', 'trophies', 'medals'],
 ];
