8ed748597ad2f30b7fc960d07af8a7191cb28e07
Lex Postma <lex@postma.me>
2022-04-06 22:46:38 +0200

    Another minor tweak

--- includes/variables.php
+++ includes/variables.php
@@ -61,7 +61,7 @@ if ($environment == 'production') {
 	$head_defaults['site_goatcounter'] = 'https://rickies.goatcounter.com';
 	$github = 'main';
 	// NOTE: Update for new releases
-	$refresh = 'v2.2.2';
+	$refresh = 'v2.2.3';
 } else {
 	$appIconDirectory = '/images/app-icons/' . $environment . '/';
 	$head_defaults['site_goatcounter'] = 'https://rickies-test.goatcounter.com';
