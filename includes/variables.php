853d884fdc41bd8200b1248c9f6b26d714c14f74
Lex Postma <lex@postma.me>
2022-06-01 15:04:32 +0200

    Fix styling of pick notes with ul-list

--- includes/variables.php
+++ includes/variables.php
@@ -61,7 +61,7 @@ if ($environment == 'production') {
 	$head_defaults['site_goatcounter'] = 'https://rickies.goatcounter.com';
 	$github = 'main';
 	// NOTE: Update for new releases
-	$refresh = 'v2.3.4';
+	$refresh = 'v2.3.5';
 } else {
 	$appIconDirectory = '/images/app-icons/' . $environment . '/';
 	$head_defaults['site_goatcounter'] = 'https://rickies-test.goatcounter.com';
