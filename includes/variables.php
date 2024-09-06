aec49f0f6d880da4890ae09048097a2a613d0277
Lex Postma <lex@postma.me>
2024-09-06 22:18:51 +0200

    Bump version

--- includes/variables.php
+++ includes/variables.php
@@ -51,7 +51,7 @@ if ($environment == 'production') {
 	$head_defaults['site_goatcounter'] = 'https://rickies.goatcounter.com';
 	$github = 'main';
 	// NOTE: Update for new releases
-	$refresh = 'v2.6.0';
+	$refresh = 'v2.6.1';
 } else {
 	$appIconDirectory = '/images/app-icons/' . $environment . '/';
 	$head_defaults['site_goatcounter'] = 'https://rickies-test.goatcounter.com';
